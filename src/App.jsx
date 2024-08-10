76bca20f7e025771ae06f1d3f820f394b167ca20
Sharathpatel31 <chintapoolasharath@gmail.com>
2024-08-10 11:37:42 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material/styles';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import { createTheme } from '@mui/material/styles';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Users from './pages/UserListing';
